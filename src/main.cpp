a2bbabd7748365389a15de064f02287b45b6c183
aiwa <aiwavision@protonmail.com>
2019-11-08 21:02:29 -0300

    cleaned a bit

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,13 @@
 
 int main(void){
     int size{};
-    /*
-    if (argc != 2)     
-        throw std::runtime_error("INVALID NUMBER OF ARGUMENTS\n");
-    std::string wrapper(argv[1]);   //not gonna use c-strings
-    size = std::stoi(wrapper);*/
-    // uncomment it, and comment/delete the code above this to use it
     std::cin >> size;
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::cin.clear();
     std::ios::sync_with_stdio(false);   // i was scared at the possibility of a slow output
     // this adds the need to flush the stream to output to be displayed
-    // good idea? idk
-    Graph test(size);
-    //std::cout << "Please enter data:" << std::endl;
-    test.read_keyboard_to_graph();
-    //std::cout << test;
-    std::cout << test.minimum_biforest() << std::endl;
+    Graph measure(size);
+    measure.read_keyboard_to_graph();
+    std::cout << measure.minimum_biforest() << std::endl;
     return 0;
 }

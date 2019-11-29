fa74ec1512095cdb1ff46c2dd040261b1126d297
Agatha <aiwavision@protonmail.com>
2019-11-29 14:26:20 -0300

    now it's right

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,6 @@ int main(void) {
     std::cin >> size;
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     std::cin.clear();
-    std::ios::sync_with_stdio(false);  // i was scared at the possibility of a slow output
-    // this adds the need to flush the stream to output to be displayed
     Graph measure(size);
     measure.read_keyboard_to_graph();
     std::cout << measure.minimum_biforest() << std::endl;
